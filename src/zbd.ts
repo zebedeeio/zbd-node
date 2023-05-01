3a23a10e0af8d5fe1377be50265b3047dd5940b5
Andre Neves <andre@zebedee.io>
2023-05-01 13:09:51 -0700

    refactor: working on sdk error handling

--- src/zbd.ts
+++ src/zbd.ts
@@ -45,6 +45,11 @@ class zbd {
         'Content-Type': 'application/json',
       },
     });
+
+    this.axiosClient.interceptors.response.use(
+      (response: any) => response,
+      (error: any) => error.response,
+    );
   }
 
   async createCharge(options: ChargeOptionsType) {
@@ -160,22 +165,6 @@ class zbd {
   
       return response;
     } catch (error: any) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
       throw error;
     }
   }
