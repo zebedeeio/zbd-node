d782f2b64dc82c0b499f02157159116d41a52025
Andre Neves <andre@zebedee.io>
2023-05-01 12:33:10 -0700

    chore: update axios throwing

--- src/zbd.ts
+++ src/zbd.ts
@@ -32,7 +32,7 @@ import {
 class zbd {
   apiKey: any;
   apiBaseUrl: any;
-  axiosClient: any;
+  axiosClient: any;  
 
   constructor(apiKey: any) {
     this.apiKey = apiKey;
@@ -165,9 +165,23 @@ class zbd {
   
       return response;
     } catch (error: any) {
-      console.log({ error });
-      return error;
-      throw new Error(error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+      throw error;
     }
   }
   
